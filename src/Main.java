643a4b1b3e8eb5c5d1bfe1f96eb9d8952c4df181
ayzataibekova-svg <ayzataibekova@gmail.com>
2026-09-25 02:07:04 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,1 @@
-import java.util.Scanner;
-public class Main {
 
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int n=in.nextInt();
-        System.out.println("c " + n + " is " + (n+1) + ".");
-        System.out.println("v " + n + " is " + (n-1) + ".");
-    }
-
-}
