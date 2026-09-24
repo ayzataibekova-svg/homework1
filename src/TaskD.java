8278c1247aeaf8b818b33cde63faa11c4efbef1a
Student <ayzataibekova@gmail.com>
2026-09-24 13:22:10 +0600

    ferst commit

---
+++ src/TaskD.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TaskD {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int N=in.nextInt();
+        int K=in.nextInt();
+
+        System.out.println(K%N);
+    }
+
+}
+
