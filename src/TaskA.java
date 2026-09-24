ed6d1414781b8c3fdcbc0e85cb1df9c6e01d231c
aizat <ayzataibekova@gmail.com>
2026-09-25 01:37:01 +0600

    ferst commit

---
+++ src/TaskA.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TaskA {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int a= in.nextInt();
+        int b= in.nextInt();
+        double c= Math.sqrt(a*a+b*b);
+        System.out.println(c);
+    }
+
+
+}
