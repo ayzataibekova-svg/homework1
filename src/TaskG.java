41ac743a6ae09ea94a1b86325eafaa8318601227
aizat <ayzataibekova@gmail.com>
2026-09-25 09:22:47 +0600

    new tasks

---
+++ src/TaskG.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class TaskG {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int a= in.nextInt();
+        System.out.println(a/10);
+    }
+}
---
+++ src/TaskH.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class TaskH {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int b= in.nextInt();
+        System.out.println((b/10)%10);
+    }
+}
